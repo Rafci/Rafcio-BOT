8cb3d2b6ad11f6e395c4ac49efcd9439ee01fd78
Rafci <46029595+Rafci@users.noreply.github.com>
2020-04-04 19:33:27 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,52 @@
+
+
+
+
+
+
+
+
+
+
+    
+    msg.reply('Pong!');
+  console.log(`${client.user.tag} My bot ready`);
+  if (msg.content === 'tes') {
+  }
+
+.addField('Moderation', 'ban-kick-mute-unmute-unban')
+.catch(console.error);
+.setColor('RANDOM')
+.setColor('RANDOM')
+.setDescription('__Command Are making__')
+.setDescription('__Pong Bitch__')
+.setFooter('Made with love by bot.inc')
+.setTitle('Ping Command')
+.then(presence => console.log(`Your Status has been set to ${presence.game ? presence.game.none : 'none'}`))
+client.login(process.env.TOKEN);
+client.on('message', (msg) => {
+client.on('ready', () => {
+client.on('ready', () => {
+client.user.setActivity(`Getting Developed...`, {
+console.log("Streamstatus by shawn hamby")
+console.log(`Logged in as ${client.user.tag}!`);
+const Discord = require('discord.js');
+const Embed = new Discord.RichEmbed()
+const Embed = new Discord.RichEmbed()
+const args = msg.content.slice(prefix.length).trim().split(' ');
+const client = new Discord.Client();
+const command = args.shift().toLowerCase();
+const prefix = "!!"
+if(msg.content === `${prefix}help`) {
+if(msg.content === `${prefix}ping`) {
+msg.channel.send(Embed);
+msg.channel.send(Embed);
+type: "Playing",
+url: "https://www.youtube.com"})
+}
+}
+});
+});
+}); 
+
+client.login(process.env.TOKEN);
